61d505b7f72fe73a249587618f48c3fe08648c41
Alexis Guerrero <gerreroalexis063@gmail.com>
2023-05-02 21:02:54 -0400

    placeholder data for BookList

---
+++ src/components/booksArray.js
@@ -0,0 +1,22 @@
+const booksArray = [
+  {
+    title: 'book1',
+    author: 'author1',
+    pages: 50,
+    read: true,
+  },
+  {
+    title: 'book2',
+    author: 'author2',
+    pages: 50,
+    read: false,
+  },
+  {
+    title: 'book3',
+    author: 'author3',
+    pages: 50,
+    read: true,
+  },
+]
+
+export default booksArray
