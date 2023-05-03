c70439f73977e0c1674236617b559dab298fa71c
Alexis Guerrero <gerreroalexis063@gmail.com>
2023-05-03 09:14:39 -0400

    Render header and BookList components in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import "./App.css";
+import './App.css'
+import BookList from './components/BookList'
+import Header from './components/Header'
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Header />
+      <BookList />
+    </>
+  )
 }
 
-export default App;
+export default App
