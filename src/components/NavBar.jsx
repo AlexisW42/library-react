dcb8083179b865e8044b63b00213e7dab6430cdf
Alexis Guerrero <gerreroalexis063@gmail.com>
2023-05-06 22:24:39 -0400

    Created NavBar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+function NavBar() {
+  return (
+    <nav className="grid content-center col-start-3 col-end-4">
+      <ul>
+        <li className="flex flex-row">
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+export default NavBar
