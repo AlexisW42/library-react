e2e0851c82376b11cc5a06fdd8cb95b915a12c29
Alexis Guerrero <gerreroalexis063@gmail.com>
2023-05-07 21:49:37 -0400

    Change BookList state "books" to parent component AppRouter

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,12 +1,26 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import About from './About'
 import BookList from './BookList'
+import { useEffect, useState } from 'react'
+import booksArray from './booksArray.js'
 
 function AppRouter() {
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    const books = JSON.parse(localStorage.getItem('books'))
+    if (books) {
+      setBooks(books)
+    } else {
+      console.log(booksArray)
+      setBooks(booksArray)
+    }
+  }, [])
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<BookList />} />
+        <Route path="/" element={<BookList books={books} />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -2,18 +2,9 @@ import { useEffect, useState } from 'react'
 import Book from './Book'
 import booksArray from './booksArray'
 
-function BookList() {
-  const [books, setBooks] = useState([])
+function BookList(props) {
+  const { books } = props
 
-  useEffect(() => {
-    const books = JSON.parse(localStorage.getItem('books'))
-    if (books) {
-      setBooks(books)
-    } else {
-      console.log(booksArray)
-      setBooks(booksArray)
-    }
-  }, [])
   return (
     <div className="mt-8 min-w-[90vw] max-w-6xl grid md:grid-cols-3 gap-8 justify-self-center">
       {books.map((book, index) => {
