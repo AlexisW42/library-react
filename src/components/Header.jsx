97e1051c48d32bbdc5208afcc91f503a79bc5f43
Alexis Guerrero <gerreroalexis063@gmail.com>
2023-05-10 22:50:03 -0400

    navbar enhanced

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import NavBar from './NavBar'
 
 function Header() {
   const [isOpen, setIsOpen] = useState(false)
@@ -6,10 +7,8 @@ function Header() {
   return (
     <div className="transition delay-75 h-fit mb-4 bg-indigo-900 text-gray-200 dark:bg-slate-900 dark:border-b-2 dark:border-b-slate-800">
       <nav>
-        <div className="flex flex-wrap justify-between p-4 items-center md:justify-between mx-auto">
-          <div className="px-6 justify-center col-start-1 text-3xl">
-            Library
-          </div>
+        <div className="flex flex-wrap sm:flex-nowrap justify-between p-4 items-center md:justify-between mx-auto">
+          <div className="justify-center col-start-1 text-3xl">Library</div>
           <div className="px-4 justify-self-center sm:justify-self-start col-start-2 col-end-3">
             Store information about books
           </div>
@@ -66,7 +65,7 @@ function Header() {
               type="button"
               className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
               aria-controls="navbar-search"
-              aria-expanded="false"
+              aria-expanded={`${isOpen ? 'true' : 'false'}`}
             >
               <span className="sr-only">Open menu</span>
               <svg
@@ -113,33 +112,7 @@ function Header() {
                 placeholder="Search..."
               />
             </div>
-            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-                  aria-current="page"
-                >
-                  Home
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  About
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Services
-                </a>
-              </li>
-            </ul>
+            <NavBar />
           </div>
         </div>
       </nav>
