a65370b5f1009b8c36ba61cdc12d08e14222da5d
Alexis Guerrero <gerreroalexis063@gmail.com>
2023-05-12 23:26:17 -0400

    Added a button Dark mode switcher

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
+import { useState, useEffect } from 'react'
 import Header from './components/Header'
 import AppRouter from './components/AppRouter'
 
 function App() {
+  const [darkToggle, setDarkToggle] = useState(
+    window.matchMedia('(prefers-color-scheme: dark').matches
+  )
+
+  function bodyDark() {
+    darkToggle
+      ? document.getElementsByTagName('body')[0].classList.add('dark')
+      : document.getElementsByTagName('body')[0].classList.remove('dark')
+  }
+
+  useEffect(() => {
+    bodyDark()
+  })
+
+  function ToggleDarkMode() {
+    setDarkToggle(!darkToggle)
+    bodyDark()
+  }
+
   return (
     <>
-      <Header />
+      <Header darkMode={ToggleDarkMode} darkToggle={darkToggle} />
       <AppRouter />
     </>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,53 @@
 import { useState } from 'react'
 import NavBar from './NavBar'
 
-function Header() {
+function Header(props) {
+  const { darkMode, darkToggle } = props
   const [isOpen, setIsOpen] = useState(false)
 
   return (
-    <div className="transition delay-75 h-fit mb-4 bg-indigo-900 text-gray-200 dark:bg-slate-900 dark:border-b-2 dark:border-b-slate-800">
+    <div className="h-fit mb-4 bg-indigo-900 text-gray-200 dark:bg-slate-900 dark:border-b-2 dark:border-b-slate-800">
       <nav>
         <div className="flex flex-wrap sm:flex-nowrap justify-between p-4 items-center md:justify-between mx-auto">
           <div className="justify-center col-start-1 text-3xl">Library</div>
           <div className="px-4 justify-self-center sm:justify-self-start col-start-2 col-end-3">
             Store information about books
           </div>
+          <button onClick={darkMode}>
+            {darkToggle ? (
+              <svg
+                className="w-5 ml-2 h-5 text-white"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+                ></path>
+              </svg>
+            ) : (
+              <svg
+                className="w-5 ml-2 h-5 text-white"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+                ></path>
+              </svg>
+            )}
+          </button>
           <div className="flex md:order-2">
             <button
               type="button"
