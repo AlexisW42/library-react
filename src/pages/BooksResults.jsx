87a638340d4a5e831192c598aeed4ecd6c04fe01
Alexis Guerrero <gerreroalexis063@gmail.com>
2023-09-27 09:37:28 -0400

    Created list of resulting books from data of Google Books API

--- src/pages/BooksResults.jsx
+++ src/pages/BooksResults.jsx
@@ -1,23 +1,46 @@
 import { useLayoutEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
+import ResultingBook from '../components/ResultingBook'
+import Spinner from '../components/Spinner'
 
 function BooksResults() {
   const { search } = useParams()
   const [books, setBooks] = useState(null)
+  const [error, setError] = useState(null)
 
   useLayoutEffect(() => {
     axios
       .request(`https://www.googleapis.com/books/v1/volumes?q=${search}`)
       .then((response) => {
-        setBooks(response.data)
-        console.log(response.data)
+        setBooks(response.data.items)
+        // console.log(response.data.items)
       })
       .catch((error) => {
+        setError(error)
         console.error(error)
       })
   }, [search])
 
-  return <div>BooksResults</div>
+  function evaluate() {
+    if (error) return error.message
+    else if (books != null) {
+      if (books.length === 0) {
+        return 'there is no results'
+      }
+    }
+    return <Spinner />
+  }
+
+  return (
+    <div className="self-center justify-self-center">
+      {books
+        ? books.map((book) => {
+            return <ResultingBook {...book} key={book.id} />
+          })
+        : evaluate()}
+    </div>
+  )
 }
+
 export default BooksResults
