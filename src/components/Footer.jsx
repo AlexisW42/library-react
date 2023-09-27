980e209e7a4cbf57ad799aace89fc62b911c2569
Alexis Guerrero <gerreroalexis063@gmail.com>
2023-09-27 16:13:05 -0400

    Added details to about and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,15 @@
 function Footer() {
   return (
-    <footer className="row-start-3 row-end-4 mt-4 h-fit bg-indigo-900 p-4 text-gray-200 dark:border-t-2 dark:border-t-slate-800 dark:bg-slate-900">
-      Footer
+    <footer className="row-start-3 row-end-4 mt-4 h-fit bg-indigo-900 p-4 text-center text-gray-200 dark:border-t-2 dark:border-t-slate-800 dark:bg-slate-900">
+      <p>
+        <a
+          className="text-blue-500 underline"
+          href="https://github.com/AlexisW42"
+        >
+          AlexisW42
+        </a>{' '}
+        {new Date().getFullYear()}
+      </p>
     </footer>
   )
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,32 @@
 function About() {
-  return <div>About</div>
+  return (
+    <div className="mb-8 mt-8 grid min-w-[90vw] max-w-6xl grid-flow-row self-center justify-self-center rounded-md bg-slate-50 p-8 text-left dark:bg-slate-800 dark:text-gray-200">
+      <p>
+        Web Application to store data about books in bibliographic cards. You
+        can add books, mark as read or unread and delete. The data is not
+        deleted when leaving the app, is saved locally.
+      </p>
+      <p>
+        The code of this application is availble on:{' '}
+        <a
+          className="text-blue-500 underline"
+          href="https://github.com/AlexisW42/library-react"
+        >
+          https://github.com/AlexisW42/library-react
+        </a>
+      </p>
+      <br />
+      <br />
+      <p>
+        made by{' '}
+        <a
+          className="text-blue-500 underline"
+          href="https://github.com/AlexisW42"
+        >
+          AlexisW42
+        </a>
+      </p>
+    </div>
+  )
 }
 export default About
