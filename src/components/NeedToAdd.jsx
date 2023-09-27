970f0f92026ad464b4adf3812495a4ffd3da0b03
Alexis Guerrero <gerreroalexis063@gmail.com>
2023-09-27 13:27:07 -0400

    Created meesage component when there is not yet books added

---
+++ src/components/NeedToAdd.jsx
@@ -0,0 +1,10 @@
+function NeedToAdd() {
+  console.log('hi')
+  return (
+    <div className="mb-8 mt-8 grid min-w-[90vw] max-w-6xl grid-flow-row self-center justify-self-center rounded-md bg-slate-50 p-8 text-center dark:bg-slate-800 dark:text-gray-200">
+      Need to add books <br />
+      click on + for search books
+    </div>
+  )
+}
+export default NeedToAdd
