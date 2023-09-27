2268f369581a49d7ecafc4dadce7048abcfad4f4
Alexis Guerrero <gerreroalexis063@gmail.com>
2023-09-27 13:27:42 -0400

    Added message component NeedToAdd for books

--- src/pages/BookList.jsx
+++ src/pages/BookList.jsx
@@ -1,4 +1,5 @@
 import Book from '../components/Book'
+import NeedToAdd from '../components/NeedToAdd'
 
 function BookList(props) {
   const { books, searchText } = props
@@ -11,11 +12,17 @@ function BookList(props) {
   }
 
   return (
-    <div className="mb-8 mt-8 grid min-w-[90vw] max-w-6xl gap-8 justify-self-center md:grid-cols-3">
-      {foundBooks.map((book, index) => {
-        return <Book {...book} key={index} />
-      })}
-    </div>
+    <>
+      {books.length == 0 ? (
+        <NeedToAdd />
+      ) : (
+        <div className="mb-8 mt-8 grid min-w-[90vw] max-w-6xl gap-8 justify-self-center md:grid-cols-3">
+          {foundBooks.map((book, index) => {
+            return <Book {...book} key={index} />
+          })}
+        </div>
+      )}
+    </>
   )
 }
 
